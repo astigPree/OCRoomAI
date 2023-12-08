2f54f876c930723d88f9f4b8c012d2e4395f06bd
astigPree <121760681+astigPree@users.noreply.github.com>
2023-12-08 14:52:53 +0800

    unfinished UI

---
+++ content_screens.py
@@ -0,0 +1,83 @@
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+
+from kivy.animation import Animation
+from kivy.clock import Clock
+
+from kivy.properties import ObjectProperty, ListProperty
+
+# ------------------------ Guest Screens ----------------------
+class LocationImageContainer(BoxLayout):
+    locationName : Label = ObjectProperty()
+    locationImage : Image = ObjectProperty()
+
+
+class LocationInformationContainer(BoxLayout):
+    title : Label = ObjectProperty()
+    info : Label = ObjectProperty()
+
+
+class LocationScreenInformation(Screen) :
+    enter_animate = Animation(opacity=1, duration=0.5)
+    leave_animate = Animation(opacity=0, duration=0.5)
+
+    image1 : LocationImageContainer = ObjectProperty()
+    image2 : LocationImageContainer = ObjectProperty()
+    directions : LocationInformationContainer = ObjectProperty()
+    information : LocationInformationContainer = ObjectProperty()
+
+    def on_kv_post(self, base_widget):
+        self.image1.locationName.text = "Mrs. Violeta B. Montecalvo"
+        self.information.title.text = "INFORMATION"
+        self.information.info.text = "    This room is an inventory room for all the un used supply"
+
+    def on_enter(self, *args) :
+        self.enter_animate.start(self)
+
+    def on_leave(self, *args) :
+        self.leave_animate.start(self)
+
+
+class GuestScreen(Screen) :
+    screens_handler: ScreenManager = ObjectProperty(None)
+    screens_names: list = ListProperty(['ROOM 501', 'ROOM 502', 'AVR', 'OFFICE'])
+
+    __okey_to_animate = True
+    __changing_speed = 5
+
+    def on_kv_post(self, base_widget) :
+        for name in self.screens_names :
+            location = LocationScreenInformation(name=name)
+            self.screens_handler.add_widget(location)
+
+        self.animateChangingScreens()
+
+    def animateChangingScreens(self) :
+        def animate(pos: int) :
+            if self.__okey_to_animate:
+                self.screens_handler.current = self.screens_names[pos]
+                Clock.schedule_once(lambda x : animate(pos + 1 if pos + 1 < len(self.screens_names) else 0) , self.__changing_speed)
+            else:
+                Clock.schedule_once(lambda x : animate(pos) , 1 / 30)
+
+        Clock.schedule_once(lambda x : animate(0) , self.__changing_speed)
+
+    def controlVariables(self , okey_to_animate = None, changing_speed = None):
+        if okey_to_animate is not None :
+            self.__okey_to_animate = okey_to_animate
+        if changing_speed is not None:
+            self.__changing_speed = changing_speed
+
+
+# ------------------------ Faculty Screens ----------------------
+
+class FacultyScreen(Screen) :
+    pass
+
+
+# ------------------------ Developer Screens ----------------------
+
+class DeveloperScreen(Screen) :
+    pass
--- main.py
+++ main.py
@@ -1,152 +1,64 @@
+from kivy.config import Config
+Config.set('graphics', 'fullscreen', 'auto')
+
 from kivymd.app import MDApp
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.gridlayout import MDGridLayout
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.relativelayout import MDRelativeLayout
-
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
+
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.lang.builder import Builder
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
-from kivy.clock import Clock, mainthread
+from kivy.core.text import LabelBase
+from kivy.clock import Clock
 
-from threading import Thread
-
-from activity_functions import Settings
-from recognizer import AIMouth, AIEar, AIBrain, MessageAnalyzer, NewDataSaver
+from content_screens import GuestScreen, FacultyScreen, DeveloperScreen
 
 
-class RoomInformation(Label) :
-    pass
-
-
-class RoomsLocationsPicture(MDBoxLayout) :
-    room_name: Label = ObjectProperty()
-    room_picture: Image = ObjectProperty
-    floor_name: Label = ObjectProperty()
-    floor_picture: Image = ObjectProperty()
-
-
-class Room(MDBoxLayout) :
-    room_image: Image = ObjectProperty()
-    room_name: Label = ObjectProperty()
-
-
-class RoomSelections(ScrollView) :
-    container: MDGridLayout = ObjectProperty()
-
-
-# ------------------------- Screens ------------------- #
-class FacultyScreen(Screen) :
-    pass
-
-
-class DeveloperScreen(Screen) :
-    pass
-
-
-class InformationScreen(Screen) :
-    room_locations: RoomsLocationsPicture = ObjectProperty()
-
-
-class UserScreen(Screen) :
-    room_selections: RoomSelections = ObjectProperty()
-    room_locations: RoomsLocationsPicture = ObjectProperty()
-    room_information: RoomInformation = ObjectProperty()
-
-
-# -------------------------- Main Window --------------- #
-
-class AIEmotions(MDRelativeLayout): # NOQA
-    image : Image = ObjectProperty()
-    name_holder : Label = ObjectProperty()
-    status : Label = ObjectProperty()
-
-    emotions = {} # { status : [ image , image , image ] }
-    emotion_index = NumericProperty(0)
-
-    def changeEmotions(self, interval):
-        if self.status.text == "":
-            pass
-        else:
-            self.emotion_index += 1
-            if self.emotion_index > 2:
-                self.emotion_index = 0
-
-            self.image.source = self.emotions[self.status.text][self.emotion_index]
-
-    def startAnimatingEmotions(self):
-        Clock.schedule_interval(self.changeEmotions , 1/5)
-
-
-class AIThoughts( MDRelativeLayout) :  # NOQA
-    image : Image = ObjectProperty()
-    ai_input : Label = ObjectProperty()
-
-
-class MainWindow(MDFloatLayout) :
-    app_settings: Settings = ObjectProperty()
-
-    mouth: AIMouth = ObjectProperty()  # Use to talk with text to speech
-    ear: AIEar = ObjectProperty()  # Use to record the user speech
-    brain: AIBrain = ObjectProperty()  # Use to identify the user request
-    analyzer: MessageAnalyzer = ObjectProperty()  # Use to check the precision of request based on the text
-    dataSaver: NewDataSaver = ObjectProperty()  # Use to save the transactions between computer and user for future activities
-
-    manager: ScreenManager = ObjectProperty()
-    screens_name = ( "user" , "info", "dev" , "faculty")
-
-    image : AIEmotions = ObjectProperty()
-    thoughts : AIThoughts = ObjectProperty()
+class ContentWindow(ScreenManager) :
+    listOfScreen = ('guest', 'faculty', 'developer')
 
     def on_kv_post(self, base_widget) :
-        self.manager.add_widget(UserScreen(name= self.screens_name[0] ))
-        self.manager.add_widget(InformationScreen(name=self.screens_name[1] ))
-        self.manager.add_widget(DeveloperScreen(name = self.screens_name[2] ))
-        self.manager.add_widget(FacultyScreen(name = self.screens_name[3] ))
+        self.add_widget(GuestScreen(name=self.listOfScreen[0]))
+        self.add_widget(FacultyScreen(name=self.listOfScreen[1]))
+        self.add_widget(DeveloperScreen(name=self.listOfScreen[2]))
 
-    def loadAllNecessary(self) :
-        self.app_settings = Settings()
 
-        self.mouth = AIMouth()
-        self.brain = AIBrain()
-        self.ear = AIEar()
-        # self.analyzer = MessageAnalyzer()
-        # self.dataSaver = NewDataSaver()
+class MainWindow(FloatLayout) :
+    display_talking: Label = ObjectProperty(None)
+    content: ContentWindow = ObjectProperty(None)
 
-    def mainActivityThread(self) :
-        while True :
-            pass
-            # Check the current command ( screens, unactivated, activated, shutdown )
-            # If command stop the continuation of the program, wait till owner permission
-            # If none then continue to main activity
-            # User, Faculty, Developer enter input by speech ( Microphone )
-            user_input = self.ear.captureVoice(self.app_settings.getUsedLanguage())
-            # Check if the input is in the built-in commands
-            # If in the commands then execute it and disregard future activities
+    __ai_talking: str = StringProperty("This is a test of the UI Display asjdf sdfjo jsdf jsdafj sadjo joojds oj dsf")
 
-            # If not then proceed to identification if 'who' or 'where' using neural_intents models
+    def on_kv_post(self, base_widget):
+        Clock.schedule_once(lambda x : self.animateDisplayTalking(3) , 1)
 
-            # If the input intent is 'where' identify the list of rooms in the input, if not provided ask again ( go back to 1 )
+    def animateDisplayTalking(self, talking_time: int) :
+        def Animate(speed: float) :
+            if not len(self.__ai_talking) :
+                return None
 
-            # If the input intent is 'who' identify again using neural_intents models to identify the input 'who' intent
-            # If the input is identified then show the information or the activity that the application identified
+            self.display_talking.text = self.display_talking.text + self.__ai_talking[0]
+            self.__ai_talking = self.__ai_talking[1 :]
 
-            # Wait for a minute before change back to main screen or wait till the User, Faculty, Developer input tru speech
+            Clock.schedule_once(lambda x : Animate(speed), speed)
+
+        speed = talking_time / len(self.__ai_talking)
+        Clock.schedule_once(lambda x : Animate(speed), speed)
+
+    def updateAITalking(self, text : str):
+        # TODO: before changing the ai_talking , check if the AI is done talking by checking if the ai_talking is empty string
+        self.__ai_talking = text
 
 
 class RoomAIApp(MDApp) :
-    #
-    # def on_start(self) :
-    #     Thread(target=self.root.loadAllNecessary ).start()
 
     def build(self) :
+        Builder.load_file("screens_content_design.kv")
         return Builder.load_file("design.kv")
-    #
-    # def on_stop(self):
-    #     self.root.ear.closeMicrophone()
 
 
-RoomAIApp().run()
+if __name__ == "__main__" :
+    LabelBase.register(name="ai_font", fn_regular="fonts/OpenSans-Semibold.ttf")
+    LabelBase.register(name="title_font" , fn_regular="fonts/OpenSans-Bold.ttf")
+    LabelBase.register(name="content_font" , fn_regular="fonts/OpenSans-Regular.ttf")
+    RoomAIApp().run()
