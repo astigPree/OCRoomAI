8238683647fbcd06aadc41cb5da396b8657c45a3
astigPree <121760681+astigPree@users.noreply.github.com>
2023-12-11 23:51:44 +0800

    Add files via upload

--- content_screens.py
+++ content_screens.py
@@ -6,30 +6,34 @@ from kivy.uix.image import Image
 from kivy.animation import Animation
 from kivy.clock import Clock
 
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty, ListProperty, BooleanProperty
+
 
 # ------------------------ Guest Screens ----------------------
-class LocationImageContainer(BoxLayout):
-    locationName : Label = ObjectProperty()
-    locationImage : Image = ObjectProperty()
+class LocationImageContainer(BoxLayout) :
+    locationName: Label = ObjectProperty()
+    locationImage: Image = ObjectProperty()
 
 
-class LocationInformationContainer(BoxLayout):
-    title : Label = ObjectProperty()
-    info : Label = ObjectProperty()
+class LocationInformationContainer(BoxLayout) :
+    title: Label = ObjectProperty()
+    info: Label = ObjectProperty()
 
 
 class LocationScreenInformation(Screen) :
     enter_animate = Animation(opacity=1, duration=0.5)
     leave_animate = Animation(opacity=0, duration=0.5)
 
-    image1 : LocationImageContainer = ObjectProperty()
-    image2 : LocationImageContainer = ObjectProperty()
-    directions : LocationInformationContainer = ObjectProperty()
-    information : LocationInformationContainer = ObjectProperty()
+    image1: LocationImageContainer = ObjectProperty()
+    image2: LocationImageContainer = ObjectProperty()
+    directions: LocationInformationContainer = ObjectProperty()
+    information: LocationInformationContainer = ObjectProperty()
 
-    def on_kv_post(self, base_widget):
-        self.image1.locationName.text = "Audio-Visual Room"
+    __data : dict = ObjectProperty(None)
+    isRoom : bool = BooleanProperty(True)
+
+    def on_kv_post(self, base_widget) :
+        self.image1.locationName.text = "Office of the Dean and Faculty"
         self.information.title.text = "INFORMATION"
         self.information.info.text = "    This room is an inventory room for all the un used supply"
 
@@ -39,36 +43,62 @@ class LocationScreenInformation(Screen) :
     def on_leave(self, *args) :
         self.leave_animate.start(self)
 
+    def updateScreen(self, data : dict , isRoom : bool):
+        self.__data = data
+        self.isRoom = isRoom
+        if isRoom:
+            # TODO: Display the needed data for rooms
+            self.image1.locationName.text = data["name"]
+            self.image1.locationImage.source = data["building picture"]
+            self.image2.locationName.text = data["floor"]
+            self.image2.locationImage.source = data["floor picture"]
+            self.information.info.text = data["brief information"][0]
+            self.directions.info.text = data["directions"][0]
+        else:
+            # TODO: Display the needed data for teacher
+            pass
+
+
+
 
 class GuestScreen(Screen) :
     screens_handler: ScreenManager = ObjectProperty(None)
-    screens_names: list = ListProperty(['ROOM 501', 'ROOM 502', 'AVR', 'OFFICE'])
+    screens_names: list = ListProperty([])
 
     __okey_to_animate = True
     __changing_speed = 5
 
     def on_kv_post(self, base_widget) :
-        for name in self.screens_names :
-            location = LocationScreenInformation(name=name)
-            self.screens_handler.add_widget(location)
+        pass
+
+    def on_enter(self, *args):
+        if self.parent:
+            self.parent.parent.loadGuestScreenData()
+            instructor_data = self.parent.parent.getRoomData()
+            for name in instructor_data:
+                self.screens_names.append(name)
+                location = LocationScreenInformation(name=name)
+                location.updateScreen(instructor_data[name], True)
+                self.screens_handler.add_widget(location)
 
         self.animateChangingScreens()
 
     def animateChangingScreens(self) :
 
         def animate(pos: int) :
-            if self.__okey_to_animate:
+            if self.__okey_to_animate :
                 self.screens_handler.current = self.screens_names[pos]
-                Clock.schedule_once(lambda x : animate(pos + 1 if pos + 1 < len(self.screens_names) else 0) , self.__changing_speed)
-            else:
-                Clock.schedule_once(lambda x : animate(pos) , 1 / 30)
+                Clock.schedule_once(lambda x : animate(pos + 1 if pos + 1 < len(self.screens_names) else 0),
+                                    self.__changing_speed)
+            else :
+                Clock.schedule_once(lambda x : animate(pos), 1 / 30)
 
-        Clock.schedule_once(lambda x : animate(0) , self.__changing_speed)
+        Clock.schedule_once(lambda x : animate(0), self.__changing_speed)
 
-    def controlVariables(self , okey_to_animate = None, changing_speed = None):
+    def controlVariables(self, okey_to_animate=None, changing_speed=None) :
         if okey_to_animate is not None :
             self.__okey_to_animate = okey_to_animate
-        if changing_speed is not None:
+        if changing_speed is not None :
             self.__changing_speed = changing_speed
 
 
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# from kivy.config import Config
-# Config.set('graphics', 'fullscreen', 'auto')
+from kivy.config import Config
+Config.set('graphics', 'fullscreen', 'auto')
 
 from kivymd.app import MDApp
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.label import Label
 
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, BooleanProperty, NumericProperty
 from kivy.lang.builder import Builder
 from kivy.core.text import LabelBase
 from kivy.clock import Clock
 
 from threading import Thread
+import os, pickle, json
 
 from content_screens import GuestScreen, FacultyScreen, DeveloperScreen
-
 
 
 class ContentWindow(ScreenManager) :
@@ -31,12 +31,41 @@ class MainWindow(FloatLayout) :
     content: ContentWindow = ObjectProperty(None)
     activity : Label = ObjectProperty(None)
 
+
+    size_effect = NumericProperty(5.0)
+    stop_all_running = BooleanProperty(False)
     __ai_talking: str = StringProperty("This is a test of the UI Display asjdf sdfjo jsdf jsdafj sadjo joojds oj dsf")
+
+    __instructor_data : dict = ObjectProperty(None)
+    __room_data : dict = ObjectProperty(None)
+
 
     from backend.algo_recognation import recognizeAlgo
 
+    @staticmethod
+    def loadNeededData( filename: str, folder=None, isBytes=False) -> dict :
+        filepath = os.path.join(os.path.dirname(__file__), folder, filename) if folder else os.path.join(
+            os.path.dirname(__file__), filename)
+        with open(filepath, 'rb' if isBytes else 'r') as file :
+            return json.load(file) if not isBytes else pickle.load(file)
+
+    def loadGuestScreenData(self) :
+        # TODO: Load the teachers and rooms data
+        self.__instructor_data = self.loadNeededData(filename="instructors_data.json", folder="locations_informations")
+        self.__room_data = self.loadNeededData(filename="locations_data.json", folder="locations_informations")
+
     def on_kv_post(self, base_widget):
+        self.ids['picture'].ids['picture'].source = os.path.join(os.path.dirname(__file__), 'pictures', 'building.jpg')
         Thread(target=self.recognizeAlgo).start()
+
+    def close(self):
+        self.stop_all_running = True
+
+    def getInstructorData(self) -> dict:
+        return self.__instructor_data
+
+    def getRoomData(self) -> dict :
+        return self.__room_data
 
     def animateDisplayTalking(self, talking_time: int) :
         def Animate(speed: float) :
@@ -53,12 +82,16 @@ class MainWindow(FloatLayout) :
 
     def updateAITalking(self, text : str , talking_speed : int):
         # TODO: before changing the ai_talking , check if the AI is done talking by checking if the ai_talking is empty string
+        self.display_talking.text = ""
         self.__ai_talking = text
         Clock.schedule_once(lambda x : self.animateDisplayTalking(talking_speed))
 
 
 
 class RoomAIApp(MDApp) :
+
+    def on_stop(self):
+        self.root.close()
 
     def build(self) :
         Builder.load_file("screens_content_design.kv")
